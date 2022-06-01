51db03920464dc9ab318fb5a9de599578481ad94
unknown <rakibulhasan.seu.cse@gmail.com>
2022-06-02 00:37:15 +0600

    project setup done

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP ADVANCED CRUD</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css
+    ">
+</head>
+<body>
+
+    <div class="container">
+        <h1 class="text-center py-5">PHP CRUD OPEERATION USING MODAL</h1>
+
+        <a class="btn btn-dark mt-4" href="">Add new user</a>
+      
+    </div>
+
+
+
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js
+    "></script>
+    
+</body>
+</html>
